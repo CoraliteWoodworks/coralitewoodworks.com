d6807582c478b5b9fd2b3e25f2c3e26963a46c1b
Devin Matte <devinmatte@gmail.com>
2017-08-19 22:48:38 -0400

    Moving Font awesome to CDN

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,37 +11,9 @@ module.exports = function (grunt) {
             }
         },
         cssmin: {
-            bootstrap: {
-                src: 'Coralite/static/css/bootstrap.css',
-                dest: 'Coralite/static/css/bootstrap.min.css'
-            },
-            defaultSkin: {
-                src: 'Coralite/static/css/default-skin.css',
-                dest: 'Coralite/static/css/default-skin.min.css'
-            },
-            styleMobile: {
-                src: 'Coralite/static/css/style-mobile.css',
-                dest: 'Coralite/static/css/style-mobile.min.css'
-            },
-            styleNarrow: {
-                src: 'Coralite/static/css/style-narrow.css',
-                dest: 'Coralite/static/css/style-narrow.min.css'
-            },
-            styleNarrower: {
-                src: 'Coralite/static/css/style-narrower.css',
-                dest: 'Coralite/static/css/style-narrower.min.css'
-            },
-            styleNormal: {
-                src: 'Coralite/static/css/style-normal.css',
-                dest: 'Coralite/static/css/style-normal.min.css'
-            },
-            styleWide: {
-                src: 'Coralite/static/css/style-wide.css',
-                dest: 'Coralite/static/css/style-wide.min.css'
-            },
             style: {
-                src: 'Coralite/static/css/style.css',
-                dest: 'Coralite/static/css/style.min.css'
+                src: 'Coralite/static/css/main.css',
+                dest: 'Coralite/static/css/main.min.css'
             }
         }
 
